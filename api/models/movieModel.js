cf06a53467fa7e70d5455cbbfa70934be9a14a50
Ed Ignot <edignot@gmail.com>
2020-11-23 18:40:28 -0700

    Movie model updated to support more properties

--- api/models/movieModel.js
+++ api/models/movieModel.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose')
 
 const movieSchema = mongoose.Schema({
   id: { type: Number, required: true },
-  title: { type: String, required: true },
-  poster_path: { type: String, required: true },
+  title: { type: String },
+  poster_path: { type: String },
+  backdrop_path: { type: String },
+  release_date: { type: String },
+  original_language: { type: String },
+  overview: { type: String },
+  media_type: { type: String },
+  name: { type: String },
   up_vote: { type: Number },
   down_vote: { type: Number },
 })
